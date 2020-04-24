11cd8aab23406c2f07b31afc75d48f15faf77739
kandel_k <kirill.kandel@gmail.com>
2020-04-25 01:13:01 +0300

    feat: add array for containing threads
    style: renamed variables
    
    refactor: replaced queue with array

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,21 +4,35 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Scanner;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-
-        ConcurrentLinkedQueue<Path> queue = new ConcurrentLinkedQueue<>();
+        ConcurrentIndexBuilder[] indexBuilders;
+        ArrayList<Path> files = new ArrayList<>();
+        int numOfThread;
 
         System.out.print("Enter the number of threads: ");
-        int N = new Scanner(System.in).nextInt();
+        numOfThread = new Scanner(System.in).nextInt();
 
-        createListOfFilesInFolder(queue);
+        indexBuilders = new ConcurrentIndexBuilder[numOfThread];
+
+        createListOfFiles(files);
+
+        for (int i = 0; i < numOfThread; ++i) {
+            indexBuilders[i] = new ConcurrentIndexBuilder(files);
+            try {
+                indexBuilders[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(ConcurrentIndexBuilder.getInvertedIndex());
     }
 
     private static void createListOfFiles(Collection<Path> collection) {
